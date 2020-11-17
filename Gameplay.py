4c0ffcc1089557ca7ff30d6a7fd47417bbfdb9a5
Elijah Boateng <eli.boateng60@gmail.com>
2020-11-17 22:41:02 +0000

    Create Gameplay.py

---
+++ Gameplay.py
@@ -0,0 +1,16 @@
+class GamePlay:
+    def __init__(self, rows=5, columns=5):
+        self.rows, self.columns = rows, columns
+        self.possibleMoves = []
+        # Gameboard
+        for i in range(rows):
+            print()
+            for j in range(columns):
+                print("# ", end="")
+                self.possibleMoves.append((i + 1, j + 1))
+    
+
+
+g = GamePlay()
+print()
+print(g.possibleMoves)
