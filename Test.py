c9fc4a0ae4fa109624121f925fc11e8f005a73c2
Elijah Boateng <eli.boateng60@gmail.com>
2020-11-19 21:33:30 +0000

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+import Gameplay as gp
+
+g = gp.GamePlay()
+g.loading()
+board = g.gameSetup()
+g.playFirst()
+chp = gp.GamePlay(int(board[0]), int(board[1]))
+setupWin = sg.Window("CHOMP", chp.gameBoard(int(board[0]), int(board[1])))
+event, values = setupWin.read()
+setupWin.close()
+
+while eval(event) != (1, 1):
+    chp.loading()
+    chp.getPlay(eval(event))
+    setupWin = sg.Window("CHOMP", chp.updateBoard())
+    event, value = setupWin.read()
+    setupWin.close()
+
+"""
+d = chp.playAgain()
+while d:
+    g = gp.GamePlay()
+    # chp.userDetails()
+    g.loading()
+    board = g.gameSetup()
+    chp = gp.GamePlay(int(board[0]), int(board[1]))
+    setupWin = sg.Window("CHOMP", chp.gameBoard(int(board[0]), int(board[1])))
+    event, values = setupWin.read()
+    setupWin.close()
+
+    while eval(event) != (1, 1):
+        chp.loading()
+        chp.getPlay(eval(event))
+        setupWin = sg.Window("CHOMP", chp.updateBoard())
+        event, value = setupWin.read()
+        setupWin.close()
+    d = chp.playAgain()
+"""
